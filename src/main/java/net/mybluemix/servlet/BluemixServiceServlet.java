5165f4cffb527cb51c1211a2c42bed1ce1610231
zenggb <zenggb@gmail.com>
2017-03-29 22:16:32 +0800

    update

---
+++ src/main/java/net/mybluemix/servlet/BluemixServiceServlet.java
@@ -0,0 +1,38 @@
+package net.mybluemix.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
+import com.ibm.watson.developer_cloud.language_translation.v2.model.Language;
+import com.ibm.watson.developer_cloud.language_translation.v2.model.TranslationResult;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/bluemix")
+public class BluemixServiceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().print(translateToSpanish("This is a test"));
+	}
+
+	private String translateToSpanish(String text) {
+
+		LanguageTranslation service = new LanguageTranslation();
+		service.setUsernameAndPassword("9420c40d-955a-4521-ba66-cfc5e8e03165", "G5rX2HgR2XgH");
+		TranslationResult translationResult = service.translate(text, Language.ENGLISH, Language.SPANISH).execute();
+		return translationResult.getFirstTranslation();
+	}
+
+}
